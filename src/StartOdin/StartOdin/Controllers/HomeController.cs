07def9aade855a1f933f8856156ab795f19a4627
Мусин Артём <jasebin@yandex.ru>
2024-09-13 10:46:22 +0300

    Добавление тестового проекта

---
+++ src/StartOdin/StartOdin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartOdin.Controllers;
+
+public class HomeController(ILogger<HomeController> logger) : Controller
+{
+    private readonly ILogger<HomeController> _logger = logger;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+}
